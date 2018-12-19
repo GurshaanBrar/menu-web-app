75c9c523b884b3bb90c396d6a6e867aef5e78a47
Kai Sackville-Hii <sackvillehiik@gmail.com>
2018-12-18 21:50:21 -0800

    env.js added to support private api keys

--- src/App/Services/RequestHandler.js
+++ src/App/Services/RequestHandler.js
@@ -1,18 +1,12 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import { Config } from '../../env';
 
 export default class RequestHandler {
 
     static initFirebase() {
         // Initialize Firebase
-        var config = {
-            apiKey: "AIzaSyA05wLZQbEq5QDWPvIGfTAFX0uTf-9zj8I",
-            authDomain: "reactive-solutions-menu-app.firebaseapp.com",
-            databaseURL: "https://reactive-solutions-menu-app.firebaseio.com",
-            projectId: "reactive-solutions-menu-app",
-            storageBucket: "reactive-solutions-menu-app.appspot.com",
-            messagingSenderId: "83679593588"
-        };
+        var config = Config;
         firebase.initializeApp(config);
 
         // Initialize Cloud Firestore through Firebase
@@ -24,57 +18,58 @@ export default class RequestHandler {
     }
     
     // @Def: sets document with body, if doc dne then it will create it
-    // @Param: col = target collection
-    //         doc = target document
-    //         body = json object to write
+    // @Param: col = target collection (string)
+    //         doc = target document (string)
+    //         body = json object to write (object)
     //       edit the existing object, else it will make a new obj.
     // @Post: returns promise
-    static setDocument(col: string, doc: string, body: Object) {    
+    static setDocument(col, doc, body) {    
         return(
             this.fs.collection(col).doc(doc).set(body)
         )
     }
 
     // @Def: Updates key with new value
-    // @Param: col = target collection
-    //         doc = target document
-    //         body = contains key value pair to change
+    // @Param: col = target collection (string)
+    //         doc = target document (string)
+    //         body = contains key value pair to change (object)
     // @Post: returns promise
-    static updateDocument(col: string, doc: string, body: Object) {    
+    static updateDocument(col, doc, body) {    
         return(
             this.fs.collection(col).doc(doc).update(body)
         )
     }
 
     // @Def: gets a document 
-    // @Param: col = target collection
-    //         doc = target document
+    // @Param: col = target collection (string)
+    //         doc = target document (string)
     // @Post: returns promise
-    static getDocument(col: string, doc: string) {
+    static getDocument(col, doc) {
         return(
             this.fs.collection(col).doc(doc).get()
         )
     }
 
     // @Def: gets documents
-    // @Param: col = target collection
-    //         query = query expression
-    //         queryKey = key in expression
-    //         queryValue = value in expression
+    // @Param: col = target collection (string)
+    //         query = query expression (string)
+    //         queryKey = key in expression (string)
+    //         queryValue = value in expression (string)
+    //         limit = limit to number of docs returned (number)
     // @Post: returns promise
-    static findDocuments(col: string, queryKey: string, query: string, queryValue: any, limit: number) {
+    static findDocuments(col, queryKey, query, queryValue, limit) {
         return(
             this.fs.collection(col).where(queryKey, query, queryValue).limit(limit).get()
         )
     }
 
     // @Def: Appends to Document Array, note that document must exist
-    // @Param: col = target collection
-    //         doc = target document
-    //         updateKey = key to change
-    //         updateBody = value to change
+    // @Param: col = target collection (string)
+    //         doc = target document (string)
+    //         updateKey = key to change (string)
+    //         updateBody = value to change (object)
     // @Post: returns promise
-    static appendToArray(col: string, doc: string, updateKey:string, updateBody ) {
+    static appendToArray(col, doc, updateKey, updateBody ) {
         var tempObj = {};
         tempObj[updateKey] = firebase.firestore.FieldValue.arrayUnion(updateBody);
 
